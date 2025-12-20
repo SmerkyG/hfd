21a4c038583cefc627ded6817765af9dc7026142
SmerkyG <SmerkyG@users.noreply.github.com>
2025-12-19 20:57:15 -0800

    new trainer

---
+++ model/configuration_qwen3_rwkv7c_hybrid.py
@@ -0,0 +1,3 @@
+from model.wrap_hf import create_config_class
+
+Qwen3RWKV7cHybridConfig = create_config_class(base_configuration_path='transformers.models.qwen3.configuration_qwen3.Qwen3Config', )
--- model/wrap_hf.py
+++ model/wrap_hf.py
@@ -3,10 +3,11 @@ import torch.nn as nn
 from typing import Optional, Tuple, Union, Any
 from transformers import Cache
 from contextlib import contextmanager
+from torch.utils.checkpoint import checkpoint as torch_checkpoint
 
-def radlads_replacement_attn(module, *args, **kwargs):
+def radlads_replacement_attn(module, query, key, value, attention_mask, **kwargs):
     # forward while removing underscores we added to certain kwargs
-    return module.attn_replacement(*args, **{k.rstrip('_'):v for k,v in kwargs.items()}), None
+    return module.attn_replacement(query, key, value, attention_mask, **{k.rstrip('_'):v for k,v in kwargs.items()}), None
 
 from transformers.modeling_utils import AttentionInterface
 AttentionInterface.register('radlads_replacement_attn', radlads_replacement_attn)
@@ -41,7 +42,7 @@ def create_model_class(replacement_attention_class, base_model_path, base_attent
 
                 # in stage 1 distillation, we also have a separate teacher attention on layers that are changed
                 if getattr(config, 'radlads_distillation_stage', 0) == 1:
-                    self.teacher_attn = base_model_attention_class()
+                    self.teacher_attn = base_model_attention_class(config, layer_idx)
 
         def is_original_attention_layer(self):
             return self.config.layer_hybrid_types[self.layer_idx] == 'full_attention'
@@ -49,7 +50,6 @@ def create_model_class(replacement_attention_class, base_model_path, base_attent
         def forward(self, use_cache:bool, past_key_values:Optional[Cache], **kwargs):
             #print("RADLADSAttentionWrapper.forward")
 
-            post_attention_hidden_states = getattr(kwargs, 'post_attention_hidden_states', [])
             # add second underscored copies so things like hidden_states get passed through to attention function via kwargs
             kwargs = kwargs | dict(
                 hidden_states_=kwargs['hidden_states'],
@@ -72,6 +72,8 @@ def create_model_class(replacement_attention_class, base_model_path, base_attent
                 self.config._attn_implementation = 'radlads_replacement_attn'
 
             student_output = super().forward(use_cache=use_cache, past_key_values=past_key_values, **kwargs)
+            # FIXME - checkpointing didn't work here when I tried
+            #student_output = torch_checkpoint(super().forward, use_cache=use_cache, past_key_values=past_key_values, **kwargs, use_reentrant=False)
 
             # then revert the config
             if not self.is_original_attention_layer():
@@ -79,13 +81,19 @@ def create_model_class(replacement_attention_class, base_model_path, base_attent
 
             if self.teacher_attn is not None:
                 # in stage 1 we need to return the post attention hidden states for student and teacher, but use teacher as the actual model output for this layer
-                teacher_output = self.teacher_attn.forward(**kwargs)
+                self.teacher_attn.eval()
+                with torch.no_grad():
+                    teacher_output = self.teacher_attn.forward(use_cache=use_cache, past_key_values=past_key_values, **kwargs)
+                attention_hidden_states = kwargs.get('attention_hidden_states')
+                if attention_hidden_states is not None:
+                    attention_hidden_states += [(
+                        student_output[0] if isinstance(student_output, tuple) else student_output, 
+                        teacher_output[0] if isinstance(teacher_output, tuple) else teacher_output)]
                 layer_output = teacher_output
             else:
                 teacher_output = None
                 layer_output = student_output
-            post_attention_hidden_states += [(student_output, teacher_output)]
-
+            
             return layer_output
 
     # FIXME - would have to temporarily change GenerationMixin superclass while creating this I guess using replace_class
@@ -129,8 +137,9 @@ def create_config_class(base_configuration_path):
     base_config_class, _, _ = class_name_and_module_from_path(base_configuration_path)
 
     class RADLADSConfig(base_config_class):
-        def __init__(self, layer_hybrid_types: Optional[list[str]] = None, **kwargs):
+        def __init__(self, radlads_distillation_stage:int=0, layer_hybrid_types: Optional[list[str]] = None, **kwargs):
             super().__init__(**kwargs)
+            self.radlads_distillation_stage = radlads_distillation_stage
             if layer_hybrid_types is None:
                 layer_hybrid_types = ['replacement_attention'] * self.num_hidden_layers
             self.layer_hybrid_types = layer_hybrid_types
