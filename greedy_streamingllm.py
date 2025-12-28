828e25b30435e20bf72ff2f38ee191807f9fcefa
SmerkyG <SmerkyG@users.noreply.github.com>
2025-12-28 11:54:41 -0800

    update to simple weird method of greedy add streaming hybrid model files

--- greedy_streamingllm.py
+++ greedy_streamingllm.py
@@ -7,72 +7,6 @@ from model.wrap_hf import create_model_class, create_config_class
 from transformers.models.qwen2.modeling_qwen2 import repeat_kv
 from accelerate import init_empty_weights
 
-class StreamingLLMAttention(nn.Module):
-    def __init__(self, config, layer_idx):
-        super().__init__()
-        self.config = config
-        self.layer_idx = layer_idx
-        self.sliding_window = config.sliding_window
-
-    def forward(
-        self, 
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        attention_mask: Optional[torch.Tensor],
-        scaling: float,
-        dropout: float,
-
-        use_cache: bool,
-        hidden_states: torch.Tensor,
-        position_embeddings,
-        past_key_values,
-        cache_position,
-        **kwargs
-    ):
-        q, k, v = query, key, value
-        del query, key, value
-        B, H, L, N = q.shape
-        B, KVH, S, N = k.shape
-        
-        sdpa_kwargs = {}
-
-        if H > KVH:
-            if attention_mask is None:
-                sdpa_kwargs = {"enable_gqa": True}
-            else:
-                k = repeat_kv(k, H // KVH)
-                v = repeat_kv(v, H // KVH)
-
-        sliding_window = self.sliding_window
-        if sliding_window > 0:
-            q_idx = torch.arange(S-L, S, device=q.device)[None, None, :, None]
-            kv_idx = torch.arange(S, device=q.device)[None, None, None, :]
-            window_mask = kv_idx >= q_idx - sliding_window
-            if attention_mask is not None:
-                assert attention_mask.dtype == torch.bool
-                sink_indices = (S - attention_mask.view(B,L,S)[:,-1,:].sum(dim=-1)).view(B) # sink offset per batch idx
-                sink_mask = kv_idx == sink_indices.view(B,1,1,1)
-                #prefill_mha_mask = q_idx >= S - prefill_n_mha_tokens
-                #attention_mask = attention_mask & (sink_mask | window_mask | prefill_mha_mask)
-                attention_mask = attention_mask & (sink_mask | window_mask)
-            else:
-                sink_mask = kv_idx == 0
-                attention_mask = kv_idx <= q_idx # causal
-                attention_mask = attention_mask & (sink_mask | window_mask)
-
-        if attention_mask is not None and attention_mask.ndim == 4:
-            attention_mask = attention_mask[:, :, :, :S]
-
-        is_causal = L > 1 and attention_mask is None
-        if torch.jit.is_tracing() and isinstance(is_causal, torch.Tensor):
-            is_causal = is_causal.item()
-
-        attn_output = F.scaled_dot_product_attention(query=q, key=k, value=v, attn_mask=attention_mask, dropout_p=dropout, is_causal=is_causal, scale=scaling, **sdpa_kwargs)
-        attn_output = attn_output.transpose(1, 2).contiguous()
-
-        return attn_output
-
 import torch
 import torch.nn.functional as F
 import torch.distributed as dist
@@ -86,6 +20,8 @@ import datasets
 from dataclasses import dataclass, field
 from typing import List, Tuple, Optional, Dict, Union
 import os
+
+from model.streaming_attention import StreamingAttention
 
 @dataclass
 class TokenizingCollator:
@@ -156,9 +92,10 @@ class CLI_Config:
     base_attention_class_path:str = 'transformers.models.qwen2.modeling_qwen2.Qwen2Attention'
     base_config_class_path:str = 'transformers.models.qwen2.configuration_qwen2.Qwen2Config'
     sliding_window_size:int = 256
-    full_attention_layer_ids:list = field(default_factory=list)
+    swa_layer_ids:list = field(default_factory=list)
     seed:int = 1337
     iterate:int = 1
+    test:int = 0
 
 
 def _worker_process(local_rank:int, world_size:int, cli_config:CLI_Config):
@@ -171,24 +108,28 @@ def _worker_process(local_rank:int, world_size:int, cli_config:CLI_Config):
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
 
-    StreamingLLMHybridForCausalLM = create_model_class(
-        StreamingLLMAttention, 
+    StreamingHybridForCausalLM = create_model_class(
+        StreamingAttention, 
         cli_config.base_model_class_path,
         cli_config.base_attention_class_path,
     )
 
-    StreamingLLMHybridConfig = create_config_class(cli_config.base_config_class_path)
+    StreamingHybridConfigParent = create_config_class(cli_config.base_config_class_path)
+    class StreamingHybridConfig(StreamingHybridConfigParent):
+        def __init__(self, streaming_sliding_window:int|None = None, **kwargs):
+            super().__init__(**kwargs)
+            self.streaming_sliding_window = streaming_sliding_window
 
     if local_rank == 0: print("loading config", cli_config.model_path)
     config_dict, unused_kwargs = PretrainedConfig.get_config_dict(cli_config.model_path, _from_auto=True)
-    model_config = StreamingLLMHybridConfig.from_dict(config_dict, **unused_kwargs)
+    model_config = StreamingHybridConfig.from_dict(config_dict, **unused_kwargs)
 
     # NOTE - entirely replacement attentions, and we will change the sliding window size as needed to simulate the original model
     model_config.layer_hybrid_types = ['radlads_replacement_attention'] * model_config.num_hidden_layers
 
     if local_rank == 0: print("instantiating customized model", cli_config.model_path)
     with init_empty_weights():
-        model = StreamingLLMHybridForCausalLM(model_config)
+        model = StreamingHybridForCausalLM(model_config)
 
     if local_rank == 0: print("loading original model weights", cli_config.model_path)
     base_model = AutoModelForCausalLM.from_pretrained(cli_config.model_path, device_map=device)
@@ -200,6 +141,12 @@ def _worker_process(local_rank:int, world_size:int, cli_config:CLI_Config):
     del base_weights
 
     model.eval()
+
+    if cli_config.test:
+        model_inputs = tokenizer(["A list of colors: red, blue"], return_tensors="pt").to(device)
+        generated_ids = model.generate(**model_inputs)
+        print(tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0])
+        return
 
     dataset = datasets.load_dataset(cli_config.dataset_name)['train'] #, streaming=True)
 
@@ -229,7 +176,7 @@ def _worker_process(local_rank:int, world_size:int, cli_config:CLI_Config):
     with torch.no_grad():
         layer_count = len(model.model.layers)
         for layer_id in range(local_rank, layer_count, world_size if cli_config.iterate else 999999):
-            if cli_config.iterate and layer_id in cli_config.full_attention_layer_ids:
+            if cli_config.iterate and layer_id in cli_config.swa_layer_ids:
                 continue
             total_loss = torch.zeros([1], device=device)
             total_batchlen = 0
@@ -242,26 +189,27 @@ def _worker_process(local_rank:int, world_size:int, cli_config:CLI_Config):
                 # labels = data['labels'].to(device)
                 attention_mask = data['attention_mask'].to(device=device, dtype=torch.bool)
 
-                # change to teacher model with all full attention (no swa window)
+                # change to teacher model with all attention
                 for layer_id2 in range(model_config.num_hidden_layers):
                     model.model.layers[layer_id2].self_attn.attn_replacement.sliding_window = 0
+                # add swa layers to teacher model
+                # for layer_id2 in cli_config.swa_layer_ids:
+                #     model.model.layers[layer_id2].self_attn.attn_replacement.sliding_window = cli_config.sliding_window_size
 
                 # run teacher model
                 teacher_logits = model(input_ids).logits
 
-                # change to student model with a single additional attention layer
-                for layer_id2 in range(model_config.num_hidden_layers):
+                # change to student model with a single additional swa layer
+                for layer_id2 in cli_config.swa_layer_ids:
                     model.model.layers[layer_id2].self_attn.attn_replacement.sliding_window = cli_config.sliding_window_size
-                for layer_id2 in cli_config.full_attention_layer_ids:
-                    model.model.layers[layer_id2].self_attn.attn_replacement.sliding_window = 0
                 if cli_config.iterate:
-                    model.model.layers[layer_id].self_attn.attn_replacement.sliding_window = 0
+                    model.model.layers[layer_id].self_attn.attn_replacement.sliding_window = cli_config.sliding_window_size
 
                 # run student model
                 student_logits = model(input_ids).logits
 
-                student_logits = student_logits.masked_fill_(~attention_mask.unsqueeze(-1), -9999999)
-                teacher_logits = teacher_logits.masked_fill_(~attention_mask.unsqueeze(-1), -9999999)
+                # student_logits = student_logits.masked_fill_(~attention_mask.unsqueeze(-1), -9999999)
+                # teacher_logits = teacher_logits.masked_fill_(~attention_mask.unsqueeze(-1), -9999999)
                
                 flat_attention_mask = attention_mask.view(-1)
                 flat_student_logits = student_logits.view(-1, student_logits.size(-1))[flat_attention_mask]
